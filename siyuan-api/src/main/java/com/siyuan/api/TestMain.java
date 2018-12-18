2ed1006de34a6cdff79b5439f94aad260dc78aea
zhousiyuan0428 <32318932+zhousiyuan0428@users.noreply.github.com>
2018-12-18 16:11:35 +0800

    初始化目录结构

--- siyuan-api/src/main/java/com/siyuan/api/TestMain.java
+++
@@ -1,7 +0,0 @@
-package com.siyuan.api;
-
-public class TestMain {
-    public static void main(String[] args) {
-        System.out.println("Hello Maven jAR");
-    }
-}
---
+++ siyuan-api/src/test/java/com/siyuan/GreeterTest.java
@@ -0,0 +1,11 @@
+package com.siyuan;
+
+import org.junit.Test;
+
+public class GreeterTest {
+
+    @Test
+    public void greeterSaysHello() {
+        System.out.println("Hello ,zhousiyuan ");
+    }
+}
